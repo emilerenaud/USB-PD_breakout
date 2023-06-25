fe0222c69ac5e9b12bc45c299518faa993411925
Emile <mimil2014@gmail.com>
2023-06-24 21:58:41 -0400

    code pretty much done

--- USB-PD-Soft/src/main.cpp
+++ USB-PD-Soft/src/main.cpp
@@ -16,6 +16,8 @@
 #define CFG2 5
 #define CFG3 4
 #define PWR_EN 7
+#define outputOn HIGH
+#define outputOff LOW
 #define PG 10
 
 // Variables
@@ -25,7 +27,7 @@ enum voltage {V5, V9, V12, V15, V20};
 void LED1_task(void *pvParameters);
 void configVoltage(voltage v);
 float readVoltage(void);
-
+bool checkVoltage(voltage v);
 
 void setup() {
   // put your setup code here, to run once:
@@ -40,20 +42,22 @@ void setup() {
   digitalWrite(LED1, HIGH);
   digitalWrite(LED2, HIGH);
 
-  // digitalWrite(CFG1, LOW);
-  // digitalWrite(CFG2, LOW);
-  // digitalWrite(CFG3, 1);
-  configVoltage(V20);
-  digitalWrite(PWR_EN, HIGH);
-
-
   Serial.begin(115200);
   Serial.println("USB-PD start here");
 
+  // Config the voltage to 12V
+  voltage v = V12;
+  configVoltage(v);
+  delay(500);
+  if(checkVoltage(v))
+  {
+    Serial.println("Voltage is 12V");
+    digitalWrite(LED1, LOW);
+    digitalWrite(PWR_EN, outputOn);
+  }
+  
   // Create task to flash LED1
-  xTaskCreate(LED1_task,"LED1_task",10000,NULL,1,NULL);
-
-  // Config the voltage
+  // xTaskCreate(LED1_task,"LED1_task",10000,NULL,1,NULL);
   
 }
 
@@ -61,11 +65,17 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(digitalRead(PG) == HIGH)
   {
-    Serial.println("Power bad");
+    Serial.print("* Power bad * ");
+    digitalWrite(LED2, LOW);
+  }
+  else
+  {
+    // Serial.print("* Power good * ");
+    digitalWrite(LED2, HIGH);
   }
 
   float voltage = readVoltage();
-  Serial.print("Voltage: ");
+  Serial.print(" Voltage: ");
   Serial.println(voltage);
   delay(1000);
 }
@@ -94,7 +104,7 @@ float readVoltage() {
   }
   voltage = voltage / 10.0;
   // Transform the voltage to the real voltage
-  voltage = voltage / (1.69 / (1.69 + 10.0));
+  voltage = voltage / (1.67 / (1.69 + 10.0));
   // Serial.println(analogReadMilliVolts(VoltagePin) / (1.69 / (1.69 + 10)));
   return voltage;
 }
@@ -130,5 +140,45 @@ void configVoltage(voltage v)
       digitalWrite(CFG3, LOW);
       break;
   }
+}
+
+// Function that check if the voltage is the same as the voltage selected
+bool checkVoltage(voltage v)
+{
+  float voltage = readVoltage();
+  switch(v)
+  {
+    case V5:
+      if(voltage > 4.5 && voltage < 5.5)
+      {
+        return true;
+      }
+      break;
+    case V9:
+      if(voltage > 8.5 && voltage < 9.5)
+      {
+        return true;
+      }
+      break;
+    case V12:
+      if(voltage > 11.5 && voltage < 12.5)
+      {
+        return true;
+      }
+      break;
+    case V15:
+      if(voltage > 14.5 && voltage < 15.5)
+      {
+        return true;
+      }
+      break;
+    case V20:
+      if(voltage > 19.0 && voltage < 20.5)
+      {
+        return true;
+      }
+      break;
+  }
+  return false;
 }
 
